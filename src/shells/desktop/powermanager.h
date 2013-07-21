6454e0561a973ecf617a4d78490deefb08123408
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-07-21 21:33:33 +0200

    Power manager
    Simple object that can power off, reboot, suspend, hibernate or sleep
    the system.
    
    It will be extended with different backends such as systemd and upower.

---
+++ src/shells/desktop/powermanager.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+ * This file is part of Hawaii Shell.
+ *
+ * Copyright (C) 2013 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#ifndef POWERMANAGER_H
+#define POWERMANAGER_H
+
+#include <QtCore/QObject>
+
+namespace PowerCapability {
+    enum Capability {
+        None = 0x00,
+        PowerOff = 0x01,
+        Restart = 0x02,
+        Suspend = 0x04,
+        Hibernate = 0x08,
+        HybridSleep = 0x10
+    };
+
+    Q_DECLARE_FLAGS(Capabilities, Capability)
+    Q_DECLARE_OPERATORS_FOR_FLAGS(Capabilities)
+}
+
+class PowerManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit PowerManager(QObject *parent = 0);
+    ~PowerManager();
+
+public Q_SLOTS:
+    void powerOff();
+    void restart();
+    void suspend();
+    void hibernate();
+    void hybridSleep();
+};
+
+#endif // POWERMANAGER_H
