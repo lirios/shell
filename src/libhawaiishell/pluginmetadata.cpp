ee8a49240fac8a4bb8eb3fe8906796fdd12b16aa
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2014-01-05 09:31:06 +0100

    libhawaiishell: Say hello to PluginMetadata
    Simple class that reads plugin metadata.
    
    Issue: #122

---
+++ src/libhawaiishell/pluginmetadata.cpp
@@ -0,0 +1,189 @@
+/****************************************************************************
+ * This file is part of Hawaii Shell.
+ *
+ * Copyright (C) 2014 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include "pluginmetadata.h"
+#include "xdgdesktopfile.h"
+
+namespace Hawaii {
+
+namespace Shell {
+
+/*
+ * PluginMetadataPrivate
+ */
+
+class PluginMetadataPrivate
+{
+public:
+    PluginMetadataPrivate();
+    ~PluginMetadataPrivate();
+
+    XdgDesktopFile *entry;
+    PluginMetadata::Type type;
+};
+
+PluginMetadataPrivate::PluginMetadataPrivate()
+    : entry(nullptr)
+    , type(PluginMetadata::InvalidType)
+{
+}
+
+PluginMetadataPrivate::~PluginMetadataPrivate()
+{
+    delete entry;
+}
+
+/*
+ * PluginMetadata
+ */
+
+PluginMetadata::PluginMetadata(const QString &fileName)
+    : d_ptr(new PluginMetadataPrivate())
+{
+    Q_D(PluginMetadata);
+
+    d->entry = new XdgDesktopFile();
+    if (d->entry->load(fileName)) {
+        if (d->entry->value(QStringLiteral("Type")).toString() != QStringLiteral("Service")) {
+            // Unload metadata if it's not the right type
+            delete d->entry;
+            d->entry = nullptr;
+        } else {
+            // Determine the plugin type
+            QString type = d->entry->value(QStringLiteral("X-Hawaii-ServiceType")).toString();
+            if (type == QStringLiteral("Shell"))
+                d->type = PluginMetadata::ShellType;
+            else
+                d->type = PluginMetadata::InvalidType;
+        }
+    }
+}
+
+PluginMetadata::PluginMetadata()
+    : d_ptr(new PluginMetadataPrivate())
+{
+}
+
+PluginMetadata::~PluginMetadata()
+{
+    delete d_ptr;
+}
+
+bool PluginMetadata::isValid() const
+{
+    Q_D(const PluginMetadata);
+    return d->entry && d->entry->isValid();
+}
+
+PluginMetadata::Type PluginMetadata::type() const
+{
+    Q_D(const PluginMetadata);
+    return d->type;
+}
+
+QString PluginMetadata::name() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->name();
+}
+
+QString PluginMetadata::comment() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->comment();
+}
+
+QString PluginMetadata::iconName() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->iconName();
+}
+
+QString PluginMetadata::internalName() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->value(QStringLiteral("X-Hawaii-PluginInfo-Name")).toString();
+}
+
+QString PluginMetadata::version() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->value(QStringLiteral("X-Hawaii-PluginInfo-Version")).toString();
+}
+
+QString PluginMetadata::license() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->value(QStringLiteral("X-Hawaii-PluginInfo-License")).toString();
+}
+
+QString PluginMetadata::authorName() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->value(QStringLiteral("X-Hawaii-PluginInfo-AuthorName")).toString();
+}
+
+QString PluginMetadata::authorEmail() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->value(QStringLiteral("X-Hawaii-PluginInfo-AuthorEmail")).toString();
+}
+
+QString PluginMetadata::webSite() const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QString();
+    return d->entry->value(QStringLiteral("X-Hawaii-PluginInfo-WebSite")).toString();
+}
+
+QVariant PluginMetadata::property(const QString &key) const
+{
+    Q_D(const PluginMetadata);
+    if (!d->entry)
+        return QVariant();
+    return d->entry->localizedValue(key);
+}
+
+} // namespace Shell
+
+} // namespace Hawaii
---
+++ src/libhawaiishell/pluginmetadata.h
@@ -0,0 +1,178 @@
+/****************************************************************************
+ * This file is part of Hawaii Shell.
+ *
+ * Copyright (C) 2014 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#ifndef PLUGINMETADATA_H
+#define PLUGINMETADATA_H
+
+#include <QtCore/QString>
+#include <QtCore/QVariant>
+
+#include <HawaiiShell/Export>
+
+namespace Hawaii {
+
+namespace Shell {
+
+class PluginMetadataPrivate;
+
+/*!
+ * \brief Information about a plugin.
+ *
+ * This class holds information about a plugin.
+ */
+class HAWAIISHELL_EXPORT PluginMetadata
+{
+public:
+    enum Type {
+        InvalidType = 0,
+        ShellType
+    };
+
+    /*!
+     * Read plugin metadata from \a fileName.
+     *
+     * Plugin metadata are basically desktop entries and should be in
+     * the following form:
+     *
+     * \code
+     * [DesktopEntry]
+     * Name=User Visible Name
+     * Comment=Description of what the plugin does.
+     * Type=Service
+     *
+     * X-Hawaii-ServiceType=...
+     * X-Hawaii-PluginInfo-Name=internalname
+     * X-Hawaii-PluginInfo-Version=Version number
+     * X-Hawaii-PluginInfo-License=License name
+     * X-Hawaii-PluginInfo-AuthorName=Author's Name
+     * X-Hawaii-PluginInfo-AuthorEmail=Author's Email
+     * X-Hawaii-PluginInfo-WebSite=http://plugin.org
+     * \endcode
+     *
+     * The X-Hawaii-PluginInfo-ServiceType key can only contain
+     * one of the following values:
+     * \list
+     *   \listitem Shell
+     * \endlist
+     *
+     * Use a descriptive name for X-Hawaii-PluginInfo-License such as GPL,
+     * GPLv2, or GPLv2+ if the plugin is licensed under the terms of the
+     * GNU General Public License v2 or (at your option) any later version.
+     *
+     * Don't use nick names for X-Hawaii-PluginInfo-Author, please use
+     * your first and last name.
+     *
+     * \param fileName name with full path of the plugin metadata
+     */
+    explicit PluginMetadata(const QString &fileName);
+
+    /*!
+     * Creates an invalid plugin, with no metadata information.
+     *
+     * \sa isValid
+     */
+    PluginMetadata();
+
+    /*!
+     * Destructor.
+     */
+    ~PluginMetadata();
+
+    /*!
+     * \return whether plugin metadata information is valid
+     */
+    bool isValid() const;
+
+    /*!
+     * \return plugin type.
+     *
+     * \sa Type
+     */
+    Type type() const;
+
+    /*!
+     * \return localized user visible name.
+     */
+    QString name() const;
+
+    /*!
+     * \return localized description.
+     */
+    QString comment() const;
+
+    /*!
+     * \return icon name.
+     */
+    QString iconName() const;
+
+    /*!
+     * \return internal name.
+     */
+    QString internalName() const;
+
+    /*!
+     * \return version.
+     */
+    QString version() const;
+
+    /*!
+     * \return license name.
+     */
+    QString license() const;
+
+    /*!
+     * \return author's name.
+     */
+    QString authorName() const;
+
+    /*!
+     * \return author's email.
+     */
+    QString authorEmail() const;
+
+    /*!
+     * \return web site.
+     */
+    QString webSite() const;
+
+    /*!
+     * Returns the value associated with \a key.
+     * You can use this method if you want to read custom values.
+     * \param key which key you want to know the value
+     * \return the value associated with \a key.
+     */
+    QVariant property(const QString &key) const;
+
+private:
+    Q_DECLARE_PRIVATE(PluginMetadata)
+    PluginMetadataPrivate *const d_ptr;
+};
+
+} // namespace Shell
+
+} // namespace Hawaii
+
+#endif // PLUGINMETADATA_H
