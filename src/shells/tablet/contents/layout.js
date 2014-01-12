c49d4ab067b89256b229aad08831ff05be71f67c
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2014-01-12 18:18:34 +0100

    tablet: Say hello to the tablet shell
    For now it's just a copy of the desktop shell with a two panel layout.
    
    Issue: #122

---
+++ src/shells/tablet/contents/layout.js
@@ -0,0 +1,33 @@
+/****************************************************************************
+ * This file is part of Hawaii Shell.
+ *
+ * Copyright (C) 2014 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:GPL2+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+for (var i = 0; i < screenCount; i++) {
+    var topPanel = new Panel(i);
+    topPanel.addElement("org.hawaii.elements.indicators");
+
+    var bottomPanel = new Panel(i);
+    bottomPanel.addElement("org.hawaii.elements.launcher");
+}
