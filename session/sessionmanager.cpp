d9abe9d92c64861609bce30e30898957bc303e3c
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-03-30 01:16:18 +0200

    session: Set default QtQuick Controls style

--- session/sessionmanager.cpp
+++ session/sessionmanager.cpp
@@ -100,6 +100,7 @@ void SessionManager::setupEnvironment()
     qputenv("SAL_USE_VCLPLUGIN", QByteArray("kde"));
     qputenv("QT_PLATFORM_PLUGIN", QByteArray("Hawaii"));
     qputenv("QT_QPA_PLATFORMTHEME", QByteArray("Hawaii"));
+    qputenv("QT_QUICK_CONTROLS_STYLE", QByteArray("Aluminium"));
     qputenv("XDG_MENU_PREFIX", QByteArray("hawaii-"));
     qputenv("XDG_CURRENT_DESKTOP", QByteArray("Hawaii"));
     qputenv("XCURSOR_THEME", QByteArray("hawaii"));
