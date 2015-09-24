de9a9c13f81de3e498b2ff829c1a45d32a399ccc
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-09-24 22:53:36 +0200

    Update license

--- platformtheme/hintssettings.cpp
+++ platformtheme/hintssettings.cpp
@@ -6,46 +6,19 @@
  * Author(s):
  *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
- * $BEGIN_LICENSE:GPL3-HAWAII$
+ * $BEGIN_LICENSE:LGPL2.1+$
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3 or any later version accepted
- * by Pier Luigi Fiorini, which shall act as a proxy defined in Section 14
- * of version 3 of the license.
- *
- * Any modifications to this file must keep this entire header intact.
- *
- * The interactive user interfaces in modified source and object code
- * versions of this program must display Appropriate Legal Notices,
- * as required under Section 5 of the GNU General Public License version 3.
- *
- * In accordance with Section 7(b) of the GNU General Public License
- * version 3, these Appropriate Legal Notices must retain the display of the
- * "Powered by Hawaii" logo.  If the display of the logo is not reasonably
- * feasible for technical reasons, the Appropriate Legal Notices must display
- * the words "Powered by Hawaii".
- *
- * In accordance with Section 7(c) of the GNU General Public License
- * version 3, modified source and object code versions of this program
- * must be marked in reasonable ways as different from the original version.
- *
- * In accordance with Section 7(d) of the GNU General Public License
- * version 3, neither the "Hawaii" name, nor the name of any project that is
- * related to it, nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written
- * permission.
- *
- * In accordance with Section 7(e) of the GNU General Public License
- * version 3, this license does not grant any license or rights to use the
- * "Hawaii" name or logo, nor any other trademark.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * $END_LICENSE$
--- platformtheme/resourcehelper.h
+++ platformtheme/resourcehelper.h
@@ -7,12 +7,11 @@
  * Author(s):
  *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
- * $BEGIN_LICENSE:LGPLQt21$
+ * $BEGIN_LICENSE:LGPL213$
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) version 3.
+ * the Free Software Foundation; either version 2.1, or version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
