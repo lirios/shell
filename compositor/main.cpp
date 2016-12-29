a63c91d66182bd2756ee6d511eacddc2782bc5cf
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-12-29 15:22:08 +0100

    compositor: Set more environment variables
    Always enable Qt CSD, make sure xcb_egl is used when running with
    the xcb QPA plugin and for xdg_shell for Qt clients.

--- compositor/main.cpp
+++ compositor/main.cpp
@@ -73,6 +73,9 @@ static void setupEnvironment()
     qputenv("XCURSOR_SIZE", QByteArrayLiteral("16"));
     qputenv("XDG_MENU_PREFIX", QByteArrayLiteral("liri-"));
     qputenv("XDG_CURRENT_DESKTOP", QByteArrayLiteral("X-Liri"));
+    qputenv("QT_XCB_GL_INTEGRATION", QByteArrayLiteral("xcb_egl"));
+    qputenv("QT_WAYLAND_USE_XDG_SHELL", QByteArrayLiteral("1"));
+    qunsetenv("QT_WAYLAND_DISABLE_WINDOWDECORATION");
 
     // Load input method
     Vibe::QGSettings settings(QStringLiteral("io.liri.desktop.interface"),
