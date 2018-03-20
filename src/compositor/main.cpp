70c676f34c388db4856f9c895df95ba4ff575ff4
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-03-20 18:13:03 +0100

    Use dbus-run-session instead of dbus-launch --exit-with-session
    Documentation of dbus-launch says that for text-mode sessions (i.e.
    sessions without X11, like in our case) dbus-run-session is
    reccomended.

--- src/compositor/main.cpp
+++ src/compositor/main.cpp
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
 
     // Restart with D-Bus session if necessary
     if (qEnvironmentVariableIsEmpty("DBUS_SESSION_BUS_ADDRESS")) {
-        qCritical("No D-Bus session bus available, please run Liri Shell with dbus-launch.");
+        qCritical("No D-Bus session bus available, please run Liri Shell with dbus-run-session.");
         return 1;
     }
 
