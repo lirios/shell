143b9722b7d23e630d8c079fa9415b33b8b9234e
Liri CI <ci@liri.io>
2020-05-25 00:14:44 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/compositor/compositor_it.ts
@@ -0,0 +1,364 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<context>
+    <name>AuthDialog</name>
+    <message>
+        <location filename="../../src/compositor/qml/screens/AuthDialog.qml" line="45"/>
+        <source>Authentication required</source>
+        <translation>Autenticazione richiesta</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/AuthDialog.qml" line="175"/>
+        <source>Authenticate</source>
+        <translation>Autentica</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/AuthDialog.qml" line="182"/>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+</context>
+<context>
+    <name>ChromeMenu</name>
+    <message>
+        <location filename="../../src/compositor/qml/windows/ChromeMenu.qml" line="36"/>
+        <source>Minimize</source>
+        <translation>Riduci</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/windows/ChromeMenu.qml" line="42"/>
+        <source>Restore</source>
+        <translation>Ripristina</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/windows/ChromeMenu.qml" line="49"/>
+        <source>Maximize</source>
+        <translation>Ingrandisci</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/windows/ChromeMenu.qml" line="58"/>
+        <source>Move</source>
+        <translation>Sposta</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/windows/ChromeMenu.qml" line="63"/>
+        <source>Resize</source>
+        <translation>Ridimensiona</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/windows/ChromeMenu.qml" line="69"/>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+</context>
+<context>
+    <name>Compositor</name>
+    <message>
+        <location filename="../../src/compositor/qml/Compositor.qml" line="707"/>
+        <source>Sorry, that didn&apos;t work. Please try again.</source>
+        <translation>Qualcosa non ha funzionato. Riprova.</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorScreenView</name>
+    <message>
+        <location filename="../../src/compositor/qml/error/ErrorScreenView.qml" line="57"/>
+        <source>Oh no!</source>
+        <translation>Oh no!</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/error/ErrorScreenView.qml" line="64"/>
+        <source>Something went wrong and the desktop failed to load.</source>
+        <translation>Qualcosa non ha funzionato ed il desktop non è stato caricato.</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/error/ErrorScreenView.qml" line="75"/>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+</context>
+<context>
+    <name>FrequentAppsView</name>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/FrequentAppsView.qml" line="18"/>
+        <source>Frequent Apps</source>
+        <translation>Applicazioni frequenti</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/FrequentAppsView.qml" line="19"/>
+        <source>The apps you use frequently will show here</source>
+        <translation>Le applicazioni di uso frequente saranno mostrate qui</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherIndicator</name>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/LauncherIndicator.qml" line="34"/>
+        <source>Applications</source>
+        <translation>Applicazioni</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherMenu</name>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherMenu.qml" line="62"/>
+        <source>New Window</source>
+        <translation>Nuova finestra</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherMenu.qml" line="69"/>
+        <source>Unpin from Launcher</source>
+        <translation>Rimuovi dal lanciatore</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherMenu.qml" line="69"/>
+        <source>Pin to Launcher</source>
+        <translation>Blocca sul lanciatore</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherMenu.qml" line="81"/>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherPopOver</name>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherPopOver.qml" line="145"/>
+        <source>Type an application name...</source>
+        <translation>Scrivi il nome di un&apos;applicazione...</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherShutdownActions</name>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherShutdownActions.qml" line="45"/>
+        <source>Log out</source>
+        <translation>Disconnettiti</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherShutdownActions.qml" line="57"/>
+        <source>Lock Session</source>
+        <translation>Blocca la sessione</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/launcher/LauncherShutdownActions.qml" line="70"/>
+        <source>Power off</source>
+        <translation>Spegni</translation>
+    </message>
+</context>
+<context>
+    <name>LockScreen</name>
+    <message>
+        <location filename="../../src/compositor/qml/screens/LockScreen.qml" line="139"/>
+        <source>Sorry, wrong password. Please try again.</source>
+        <translation>Password errata. Riprova.</translation>
+    </message>
+</context>
+<context>
+    <name>LogoutScreen</name>
+    <message>
+        <location filename="../../src/compositor/qml/screens/LogoutScreen.qml" line="89"/>
+        <location filename="../../src/compositor/qml/screens/LogoutScreen.qml" line="94"/>
+        <source>Log out</source>
+        <translation>Disconnettiti</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/LogoutScreen.qml" line="90"/>
+        <source>You will be logged out in %1 seconds</source>
+        <translation>Sarai disconnesso tra %1 secondi</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/LogoutScreen.qml" line="101"/>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+</context>
+<context>
+    <name>NotificationsIndicator</name>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/NotificationsIndicator.qml" line="35"/>
+        <source>Notifications</source>
+        <translation>Notifiche</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/NotificationsIndicator.qml" line="49"/>
+        <source>No notifications</source>
+        <translation>Nessuna notifica</translation>
+    </message>
+</context>
+<context>
+    <name>PowerDialog</name>
+    <message>
+        <location filename="../../src/compositor/qml/screens/PowerDialog.qml" line="132"/>
+        <source>Power Off</source>
+        <translation>Spegni</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/PowerDialog.qml" line="152"/>
+        <source>Sleep</source>
+        <translation>Sleep</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/PowerDialog.qml" line="162"/>
+        <source>Suspend to disk</source>
+        <translation>Sospendi su disco</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/PowerDialog.qml" line="172"/>
+        <source>Power off</source>
+        <translation>Spegni</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/screens/PowerDialog.qml" line="182"/>
+        <source>Restart</source>
+        <translation>Riavvia</translation>
+    </message>
+</context>
+<context>
+    <name>PresentWindowsChrome</name>
+    <message>
+        <location filename="../../src/compositor/qml/desktop/PresentWindowsChrome.qml" line="92"/>
+        <source>Unknown</source>
+        <translation>Sconosciuto</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/compositor/application.cpp" line="121"/>
+        <source>The XDG_RUNTIME_DIR environment variable is not set.
+Refer to your distribution on how to get it, or read
+http://www.freedesktop.org/wiki/Specifications/basedir-spec
+on how to implement it.
+</source>
+        <translation>La variabile d&apos;ambiente XDG_RUNTIME_DIR non è definita.
+Riferirsi alla propria distribuzione su come impostarla, oppure leggere
+http://www.freedesktop.org/wiki/Specifications/basedir-spec
+su come implementarla.
+</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/application.cpp" line="132"/>
+        <source>The XDG_RUNTIME_DIR environment variable is set to &quot;%1&quot;, which doesn't exist.
+</source>
+        <translation>La variabile d&apos;ambiente XDG_RUNTIME_DIR è impostata a &quot;%1&quot;, che non esiste.
+</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/application.cpp" line="139"/>
+        <source>XDG_RUNTIME_DIR is set to &quot;%1&quot; and is not configured correctly.
+Unix access mode must be 0700, but is 0%2.
+It must also be owned by the current user (UID %3), but is owned by UID %4 (&quot;%5&quot;).
+</source>
+        <translation>XDG_RUNTIME_DIR è impostata a &quot;%1&quot; e non è configurata correttamente.
+La modalità di accesso Unix deve essere 0700, ma 0%2.
+Deve anche essere di proprietà dall&apos;utente corrente (UID %3), ma è di proprietà di UID %4 (&quot;%5&quot;).
+</translation>
+    </message>
+</context>
+<context>
+    <name>RunCommand</name>
+    <message>
+        <location filename="../../src/compositor/qml/desktop/RunCommand.qml" line="48"/>
+        <source>Enter a Command</source>
+        <translation>Inserisci un comando</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsIndicator</name>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="42"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="46"/>
+        <source>Size</source>
+        <translation>Dimensioni</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="53"/>
+        <source>Small</source>
+        <translation>Piccolo</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="62"/>
+        <source>Medium</source>
+        <translation>Medio</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="71"/>
+        <source>Large</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="81"/>
+        <source>Position</source>
+        <translation>Posizione</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="88"/>
+        <source>Left</source>
+        <translation>Sinistra</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="98"/>
+        <source>Top</source>
+        <translation>Sopra</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/SettingsIndicator.qml" line="108"/>
+        <source>Bottom</source>
+        <translation>Sotto</translation>
+    </message>
+</context>
+<context>
+    <name>StorageIndicator</name>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/StorageIndicator.qml" line="35"/>
+        <source>Storage</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/StorageIndicator.qml" line="50"/>
+        <source>Eject</source>
+        <translation>Espelli</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/indicators/StorageIndicator.qml" line="50"/>
+        <source>Mount</source>
+        <translation>Monta</translation>
+    </message>
+</context>
+<context>
+    <name>UnresponsiveOverlay</name>
+    <message>
+        <location filename="../../src/compositor/qml/overlays/UnresponsiveOverlay.qml" line="76"/>
+        <source>Application is not responding</source>
+        <translation>L&apos;applicazione non risponde</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/overlays/UnresponsiveOverlay.qml" line="86"/>
+        <source>This application is unresponsive, you may choose to wait a short while for it to continue or force the application to quit entirely.</source>
+        <translation>L&apos;applicazione non risponde, puoi scegliere di attendere che riprenda a funzionare oppure forzarne la chiusura.</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/overlays/UnresponsiveOverlay.qml" line="98"/>
+        <source>Wait</source>
+        <translation>Attendi</translation>
+    </message>
+    <message>
+        <location filename="../../src/compositor/qml/overlays/UnresponsiveOverlay.qml" line="103"/>
+        <source>Force Quit</source>
+        <translation>Forza chiusura</translation>
+    </message>
+</context>
+<context>
+    <name>WindowSwitcher</name>
+    <message>
+        <location filename="../../src/compositor/qml/desktop/WindowSwitcher.qml" line="53"/>
+        <source>Untitled</source>
+        <translation>Senza titolo</translation>
+    </message>
+</context>
+</TS>
