d0c58e5eaca847b44872e43fad8bac5960aa2648
Liri CI <ci@liri.io>
2021-12-08 00:36:55 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/compositor/compositor_da.ts
+++ translations/compositor/compositor_da.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="da">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="da" version="2.1">
 <context>
     <name>ChromeMenu</name>
     <message>
