202f13d5ad48927495160a750123f95b0809efe7
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2023-01-15 05:10:14 +0100

    Fix build when systemd is disabled
    Closes: #225

--- features.cmake
+++ features.cmake
@@ -12,9 +12,9 @@ set(LIRI_FEATURE_liri_development_build "$<IF:${FEATURE_liri_development_build},
 # liri-enable-systemd
 option(FEATURE_enable_systemd "Enable systemd support" ON)
 add_feature_info("Liri::Systemd" FEATURE_shell_enable_systemd "Enable systemd support")
-set(LIRI_FEATURE_shell_enable_systemd "$<IF:${FEATURE_shell_development_build},1,0>")
+set(LIRI_FEATURE_shell_enable_systemd "$<IF:${LIRI_FEATURE_shell_enable_systemd},1,0>")
 
 # shell-enable-qtquick-compiler
 option(FEATURE_shell_enable_qtquick_compiler "Build with QtQuick compiler (resulting binaries may not be compatible with future Qt releases without rebuilding them)" OFF)
 add_feature_info("Shell::QtQuickCompiler" FEATURE_shell_enable_qtquick_compiler "Build with QtQuick compiler")
-set(LIRI_FEATURE_shell_enable_qtquick_compiler "$<IF:${FEATURE_shell_development_build},1,0>")
+set(LIRI_FEATURE_shell_enable_qtquick_compiler "$<IF:${LIRI_FEATURE_shell_enable_qtquick_compiler},1,0>")
