cadde2f301c86e955a1983fcd407294138804dd5
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2024-01-17 07:52:18 +0100

    Remove old logging code

--- src/imports/shell/logging_p.h
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2018 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#pragma once
-
-#include <QtCore/QLoggingCategory>
-
-Q_DECLARE_LOGGING_CATEGORY(gLcShell)
